b0689a8114304a2e0bd09b2166e2434b379b998b
anamolhasan <mdanamolhasan1998@gmail.com>
2025-10-14 19:15:38 +0600

    middleware

---
+++ src/middleware.jsx
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server'
+import React from 'react'
+
+const middleware = (request) => {
+  return NextResponse.redirect(new URL ('/registration', request.url))
+}
+
+export default middleware
